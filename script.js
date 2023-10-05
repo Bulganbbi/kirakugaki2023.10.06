829e44e54905f1e09d7cafc340600fd78163dc45
retumaru <220109uj@yse-c.net>
2023-10-05 14:53:35 +0900

    test

---
+++ script.js
@@ -0,0 +1,10 @@
+var settingsmenu = document.querySelector(".settings-menu");
+var whiteBtn = document.getElementById("white-btn");
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height");
+
+}
+whiteBtn.onclick = function(){
+    whiteBtn.classList.toggle("white-btn-on");
+}
