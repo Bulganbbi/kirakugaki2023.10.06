896e94aad0bfc70e6a74975e12a83e1ceea62143
retumaru <220109uj@yse-c.net>
2023-10-13 14:09:39 +0900

    変更

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-// var settingsmenu = document.querySelector(".settings-menu");
-// var whiteBtn = document.getElementById("white-btn");
+var settingsmenu = document.querySelector(".settings-menu");
+var whiteBtn = document.getElementById("white-btn");
 
-// function settingsMenuToggle(){
-//     settingsmenu.classList.toggle("settings-menu-height");
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height");
 
-// }
-// whiteBtn.onclick = function(){
-//     whiteBtn.classList.toggle("white-btn-on");
-//     document.body.classList.toggle("white-theme");
-// }
+}
+whiteBtn.onclick = function(){
+    whiteBtn.classList.toggle("white-btn-on");
+    document.body.classList.toggle("white-theme");
+}
