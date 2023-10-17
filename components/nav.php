87f6b84a68372d1c850111c8753ed6f84cf25ef5
bbi <220047vd@yse-c.net>
2023-10-17 10:17:41 +0900

    10.17

--- components/nav.php
+++ components/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+<nav class="navbar navbar-expand-md navbar-dark fixed-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="./index.php"><img src="./images/logo.png" alt="Bootstrap 5 logo" class="d-inline-block align-top" width="56" height="56"></a>
             
