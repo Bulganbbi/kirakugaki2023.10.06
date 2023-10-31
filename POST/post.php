872f178a4ad4d2c676949cd16ab4386e7e3009ad
retumaru <220109uj@yse-c.net>
2023-10-31 10:50:48 +0900

    post

--- POST/post.php
+++ POST/post.php
@@ -7,11 +7,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="shortcut icon" href="./images/title.PNG" type="image/x-icon">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="../css/main.css">
 
 </head>
 <body>
-<?php include("./components/nav.php"); ?>
+<?php include("../components/setting_nav.php"); ?>
 <div class="title">
     <h2>らくがき投稿</h2>
 </div>
