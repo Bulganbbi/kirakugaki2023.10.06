57a465d4b2ea08293ed0551ae0f34dd91482ffcc
retumaru <220109uj@yse-c.net>
2023-11-09 14:28:04 +0900

    投稿画面

--- POST/delete.php
+++ POST/delete.php
@@ -3,13 +3,11 @@ require_once 'functions.php';
 
 $pdo = connectDB();
 
-$sql = 'SELECT * FROM images WHERE image_id = :image_id LIMIT 1';
+$sql = 'DELETE FROM rakugaki_images WHERE image_id = :image_id';
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':image_id', (int)$_GET['id'], PDO::PARAM_INT);
 $stmt->execute();
-$image = $stmt->fetch();
 
-header('Content-type: ' . $image['image_type']);
-echo $image['image_content'];
+header('Location:list.php');
 exit();
 ?>
--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <!--main content-->
         <div class="main-content">
             <div class="write-post-container">
-                <a href="./post_test/list.php">らくがき投稿</a>
+                <a href="./post/list.php">らくがき投稿</a>
             </div>
 
             <div class="post-container">
