9a52d82106433a41049cafa6f6e96d3280449368
retumaru <220109uj@yse-c.net>
2023-11-14 10:01:56 +0900

    post

--- POST/list.php
+++ POST/list.php
@@ -37,11 +37,15 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     <title>Image Test</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<link rel="stylesheet" href="../css/main.css">   
+    <script src="./js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
+<?php include("../components/post_nav.php"); ?>
 <div class="container mt-5">
     <div class="row">
-        <div class="col-md-8 border-right">
+        <div>
             <ul class="list-unstyled">
                 <?php for($i = 0; $i < count($images); $i++): ?>
                     <li class="media mt-5">
