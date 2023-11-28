b04d9af87316ffdb7bf4b76ff045a419d7e8d171
retumaru <220109uj@yse-c.net>
2023-11-28 11:02:07 +0900

    投稿機能

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 require_once './POST/functions.php';
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +9,6 @@ require_once './POST/functions.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="images/titlelogo.png" type="image/x-icon">
-
     <title>きらくがき</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -40,18 +36,19 @@ require_once './POST/functions.php';
     $images = $stmt->fetchAll();
     ?>
 
+    <!-- 画像表示 -->
     <?php foreach ($images as $image): ?>
         <div class="post-container">
             <div class="left-post-contents">
                 <div class="user-profile">
                     <img src="images/cat-01.jpg">
                     <div>
-                        <p><?= $image['username']; ?></p>
+                        <!-- ユーザーネーム -->
                     </div>
                 </div>
             </div>
             <!-- 投稿テキスト表示 -->
-            <p class="post-text"><?= $image['image_comment']; ?><a href="#">#新タイプ</a></p>
+            <p class="post-text"><?= $image['image_comment']; ?><a href="#"><?= $image['image_hashtag']; ?></a></p>
             
             <!-- データベースから取得した画像を表示 -->
             <img src="data:image/<?= $image['image_type']; ?>;base64,<?= base64_encode($image['image_content']); ?>" class="post-img">
@@ -60,5 +57,5 @@ require_once './POST/functions.php';
     <?php endforeach; ?>
 </body>
     <script src="./js/script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>]
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
