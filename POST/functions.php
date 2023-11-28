f8b84b01bdb6846954a0475e6fdf67f8e8db43b5
retumaru <220109uj@yse-c.net>
2023-11-28 09:17:12 +0900

    エラー修正

--- POST/functions.php
+++ POST/functions.php
@@ -3,7 +3,7 @@
 function connectDB() {
     $param = 'mysql:dbname=kirakugaki;host=localhost';
     try {
-        $pdo = new PDO($param, 'kirakugaki', "kirakugaki");
+        $pdo = new PDO($param, 'kirakugaki', "");
         return $pdo;
 
     } catch (PDOException $e) {
