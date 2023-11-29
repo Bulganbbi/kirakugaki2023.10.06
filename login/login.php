719fc63ca697eebb0c47f6778bde5f27ec73e422
retumaru <220109uj@yse-c.net>
2023-11-29 17:01:32 +0900

    login

--- login/login.php
+++ login/login.php
@@ -1,51 +1,82 @@
 <?php
 require_once '../POST/functions.php';
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="author" content="Devcrud">
-    <title>ログイン</title>
-	<link rel="stylesheet" href="../css/signin.css">
-    <link rel="shortcut icon" href="assets/imgs/title.PNG" type="image/x-icon">
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-</head>
-<body>
-    <div class="wrapper">
-    <form action="login.php" method="post">
-        <h1>ログイン</h1>
-        <div class="input-box">
-            <input type="email" name="email" placeholder="メールアドレス" required>
-        </div>
-        <div class="input-box">
-            <input type="password" name="password" placeholder="パスワード" required>
-        </div>
-        <div class = "remember-forget">
-            <label><input type="checkbox">覚えておく</label>
-            <a href="#">パスワード忘れた方</a>
-        </div>
-        <button type="submit" class="btn">ログイン</button>
-
-        <div class="register-link">
-            <p>アカウントを持ってない方 <a href="signin.php">こちら</a></p>
-        </div>
-    </form>
-<?php
+    // データベースへの接続
     $con = new mysqli("localhost", "kirakugaki", "", "kirakugaki");
 
     if ($con->connect_error) {
         die("Failed to connect: " . $con->connect_error);
     }
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT, ['cost' => 12]);
+
+    // プリペアドステートメントの作成
+    $stmt = $con->prepare("SELECT id, password FROM users WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($user_id, $hashed_password);
+        $stmt->fetch();
+
+        // パスワードの照合
+        if (password_verify($password, $hashed_password)) {
+            // ログイン成功時の処理（セッションの設定など）
+            session_start();
+            $_SESSION['user_id'] = $user_id;
+
+            // ./index.php にリダイレクト
+            header("Location: ../index.php");
+            exit();
+        } else {
+            echo "<h2>パスワードが間違っています。</h2>";
+        }
+    } else {
+        echo "<h2>メールアドレスが見つかりません。</h2>";
     }
 
+    // ステートメントを閉じる
+    $stmt->close();
+
+    // 接続を閉じる
     $con->close();
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Devcrud">
+    <title>ログイン</title>
+    <link rel="stylesheet" href="../css/signin.css">
+    <link rel="shortcut icon" href="assets/imgs/title.PNG" type="image/x-icon">
+</head>
+<body>
+    <div class="wrapper">
+        <form action="login.php" method="post">
+            <h1>ログイン</h1>
+            <div class="input-box">
+                <input type="email" name="email" placeholder="メールアドレス" required>
+            </div>
+            <div class="input-box">
+                <input type="password" name="password" placeholder="パスワード" required>
+            </div>
+            <div class="remember-forget">
+                <label><input type="checkbox">覚えておく</label>
+                <a href="#">パスワード忘れた方</a>
+            </div>
+            <button type="submit" class="btn">ログイン</button>
+            <div class="register-link">
+                <p>アカウントを持ってない方 <a href="signin.php">こちら</a></p>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- login/signin.php
+++ login/signin.php
@@ -1,7 +1,13 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once '../POST/functions.php';
 
 $con = new mysqli("localhost", "kirakugaki", "", "kirakugaki");
+
+session_start();
+session_regenerate_id(true);
 
 if ($con->connect_error) {
     die("Failed to connect: " . $con->connect_error);
@@ -48,17 +54,37 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // created_at および updated_at に現在の日時を挿入
             $now = date("Y-m-d H:i:s");
 
-            // データベースに挿入
-            $insert_stmt = $con->prepare("INSERT INTO users (name, email, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?)");
-            $insert_stmt->bind_param("sssss", $name, $email, $hashedPassword, $now, $now);
+            // アイコン画像の処理
+            $user_icon = null;
 
+            if (isset($_FILES['icon']) && $_FILES['icon']['error'] === UPLOAD_ERR_OK) {
+                $user_icon = file_get_contents($_FILES['icon']['tmp_name']);
+            } else {
+                // デフォルトのアイコンを使用
+                $default_icon_path = '../images/initial.jpg';
+                $user_icon = file_get_contents($default_icon_path);
+            }
+
+            // データベースに挿入
+            $insert_stmt = $con->prepare("INSERT INTO users (name, email, password, user_icon, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)");
+            $insert_stmt->bind_param("ssssss", $name, $email, $hashedPassword, $user_icon, $now, $now);
+
+
+            // クエリの実行
             if ($insert_stmt->execute()) {
                 echo "<h2>アカウントが作成されました。</h2>";
             } else {
                 echo "<h2>アカウントの作成に失敗しました。</h2>";
+                echo "エラーメッセージ: " . $insert_stmt->error;
             }
 
             $insert_stmt->close();
+        }
+    } else {
+        // エラーメッセージを表示
+        echo "<h2>入力エラーがあります:</h2>";
+        foreach ($errors as $error) {
+            echo "<p>{$error}</p>";
         }
     }
 }
@@ -66,9 +92,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 $con->close();
 ?>
 
-<!-- 以下はHTMLのフォーム部分（省略） -->
-
-
+<!-- 以下はHTMLのフォーム部分 -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -81,16 +105,22 @@ $con->close();
 </head>
 <body>
     <div class="wrapper">
-        <form action="signin.php" method="post">
+        <form action="signin.php" method="post" enctype="multipart/form-data">
             <h1>サインイン</h1>
+            <!-- ユーザ名の入力フィールドを追加 -->
             <div class="input-box">
                 <input type="text" name="name" placeholder="ユーザ名" required>
             </div>
+            <!-- 既存のメールアドレスとパスワードの入力フィールド -->
             <div class="input-box">
                 <input type="email" name="email" placeholder="メールアドレス" required>
             </div>
             <div class="input-box">
                 <input type="password" name="password" placeholder="パスワード" required>
+            </div>
+            <!-- アイコン画像のアップロードフィールド -->
+            <div class="input-box">
+                <input type="file" name="icon" accept="image/*">
             </div>
             <div class="remember-forget">
                 <label><input type="checkbox">覚えておく</label>
