f2ab50b3a7b2ada3702c642403d3a81620767338
bbi <220047vd@yse-c.net>
2023-12-08 13:28:20 +0900

    social icon

--- profile.php
+++ profile.php
@@ -38,7 +38,7 @@ $con->close();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'><link rel="stylesheet" href="./style.css">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'><link rel="stylesheet" href="./main.css">
 </head>
 
 <body>
@@ -60,13 +60,14 @@ $con->close();
                         プロフィール
                     </p>
                     <hr>
-                    <ul>
-                        <a href="https://www.facebook.com/"><i class='bx bxl-facebook'></i></a>
-                        <a href="https://www.instagram.com/"><i class='bx bxl-instagram' ></i></a>
-                        <a href="https://www.pinterest.com/"><i class='bx bxl-pinterest'></i></a>
-                        <a href="https://twitter.com/"><i class='bx bxl-pinterest'></i></a>
-                        
-                    </ul>
+                    <div class="social-icon">
+                        <ul>
+                            <li><a href="#"><i class="fa-brands fa-instagram instagram"></i></a></li>
+                            <li><a href="#"><i class="fa-brands fa-twitter twitter"></i></a></li>
+                            <li><a href="#"><i class="fa-brands fa-pinterest pinterest"></i></a></li>
+
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div class="photos">
