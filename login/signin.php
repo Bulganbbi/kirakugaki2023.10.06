4ea1c984d79583575178378baf44b4f4d48bde62
retumaru <220109uj@yse-c.net>
2023-12-12 16:09:31 +0900

    up

--- login/signin.php
+++ login/signin.php
@@ -4,7 +4,6 @@ ini_set('display_errors', 1);
 
 require_once '../POST/functions.php';
 
-
 $con = new mysqli("localhost", "kirakugaki", "", "kirakugaki");
 
 session_start();
@@ -52,14 +51,58 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $user_icon = null;
 
             if (isset($_FILES['icon']) && $_FILES['icon']['error'] === UPLOAD_ERR_OK) {
-                $user_icon = file_get_contents($_FILES['icon']['tmp_name']);
+                $tempFilePath = $_FILES['icon']['tmp_name'];
+            
+                $image_info = getimagesize($tempFilePath);
+                $allowed_mime_types = ['image/jpeg', 'image/png'];
+            
+                if (!in_array($image_info['mime'], $allowed_mime_types)) {
+                    die("JPEGまたはPNG形式の画像を選択してください");
+                }
+            
+                $file_extension = strtolower(pathinfo($_FILES['icon']['name'], PATHINFO_EXTENSION));
+                $outputFilePath = $_SERVER['DOCUMENT_ROOT'] . '/-main2222/kirakugaki2023.10.06/processed_image/user_icon_' . uniqid() . '.' . $file_extension;
+            
+                switch ($file_extension) {
+                    case 'jpeg':
+                    case 'jpg':
+                        $image = imagecreatefromjpeg($tempFilePath);
+                        break;
+                    case 'png':
+                        $image = imagecreatefrompng($tempFilePath);
+                        break;
+                    default:
+                        die("サポートされていない画像形式です");
+                }
+            
+                $croppedImage = imagecrop($image, ['x' => 0, 'y' => 0, 'width' => 100, 'height' => 100]);
+                ob_start();
+            
+                // 画像形式によって保存処理を変更
+                switch ($file_extension) {
+                    case 'jpeg':
+                    case 'jpg':
+                        imagejpeg($croppedImage, $outputFilePath);
+                        break;
+                    case 'png':
+                        imagepng($croppedImage, $outputFilePath);
+                        break;
+                    default:
+                        die("サポートされていない画像形式です");
+                }
+            
+                $user_icon = ob_get_clean();
+            
+                imagedestroy($image);
+                imagedestroy($croppedImage);
             } else {
                 $default_icon_path = '../images/initial.jpg';
                 $user_icon = file_get_contents($default_icon_path);
             }
 
-            $insert_stmt = $con->prepare("INSERT INTO users (name, email, password, user_icon, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)");
-            $insert_stmt->bind_param("ssssss", $name, $email, $hashedPassword, $user_icon, $now, $now);
+            // 保存先のファイルパスをデータベースに保存
+            $insert_stmt = $con->prepare("INSERT INTO users (name, email, password, user_icon, created_at, updated_at, user_icon_type) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $insert_stmt->bind_param("sssssss", $name, $email, $hashedPassword, $user_icon, $now, $now, $file_extension);
 
             if ($insert_stmt->execute()) {
                 // アカウントが作成された場合の処理
@@ -83,6 +126,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 $con->close();
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -92,59 +136,109 @@ $con->close();
     <title>アカウント作成</title>
     <link rel="stylesheet" href="../css/signin.css">
     <link rel="shortcut icon" href="assets/imgs/title.PNG" type="image/x-icon">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'><link rel="stylesheet" href="./style.css">
-    <script>
-        function previewImage(input) {
-            var preview = document.getElementById('imagePreview');
-            var file = input.files[0];
-            var reader = new FileReader();
-
-            reader.onloadend = function () {
-                preview.src = reader.result;
-            };
-
-            if (file) {
-                reader.readAsDataURL(file);
-            } else {
-                preview.src = "";
-            }
-        }
-    </script>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'>
+    <!-- CroppieのファイルをCDN経由で読み込む -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.js"></script>
+    
     <style>
         .wrapper {
             text-align: center;
         }
 
         #imagePreview {
-            border-radius: 50%;
             max-width: 150px;
             max-height: 150px;
             margin: 10px auto;
             display: block;
         }
+
+        #croppieContainer {
+            margin: 10px auto;
+        }
     </style>
+
+    <script>
+        function initCroppie() {
+            var image = document.getElementById('imagePreview');
+            var croppieContainer = document.getElementById('croppieContainer');
+            var croppedImage = document.getElementById('croppedImage');
+            
+            var croppie = new Croppie(croppieContainer, {
+                viewport: { width: 100, height: 100, type: 'circle' },
+                boundary: { width: 150, height: 150 }
+            });
+
+            function handleImageSelect(event) {
+                var file = event.target.files[0];
+                if (file) {
+                    var reader = new FileReader();
+                    reader.onload = function (e) {
+                        // 画像の読み込みが完了したらCroppieにセット
+                        croppie.bind({
+                            url: e.target.result
+                        });
+
+                        // Croppieの表示
+                        if (image) {
+                            image.style.display = 'none';
+                        }
+                        if (croppieContainer) {
+                            croppieContainer.style.display = 'block';
+                        }
+                        if (croppedImage) {
+                            croppedImage.style.display = 'none';
+                        }
+                    };
+                    reader.readAsDataURL(file);
+                }
+            }
+
+            document.getElementById('uploadBtn').addEventListener('change', handleImageSelect);
+
+            document.getElementById('cropBtn').addEventListener('click', function () {
+                croppie.result({
+                    type: 'base64',
+                    size: { width: 100, height: 100 }
+                }).then(function (base64) {
+                    // 画像を表示する要素が存在するか確認
+                    if (croppedImage) {
+                        croppedImage.src = base64;
+                        if (image) {
+                            image.style.display = 'none';
+                        }
+                        if (croppieContainer) {
+                            croppieContainer.style.display = 'none';
+                        }
+                        if (croppedImage) {
+                            croppedImage.style.display = 'block';
+                        }
+                    }
+                });
+            });
+        }
+    </script>
 </head>
-<body>
+<body onload="initCroppie()">
     <div class="wrapper">
         <form action="signin.php" method="post" enctype="multipart/form-data">
             <h1>アカウント作成</h1>
-            <!-- ユーザ名の入力追加 -->
             <div class="input-box">
                 <input type="text" name="name" placeholder="ユーザ名" required>
             </div>
-            <!-- 既存のメールアドレスとパスワードの入力フィールド -->
             <div class="input-box">
                 <input type="email" name="email" placeholder="メールアドレス" required>
             </div>
             <div class="input-box">
                 <input type="password" name="password" placeholder="パスワード" required>
             </div>
-            <!-- アイコン画像のアップロードフィールド -->
             <div class="input-box">
-                <input type="file" name="icon" id="uploadBtn" accept="image/*" onchange="previewImage(this);">
+                <input type="file" name="icon" id="uploadBtn" accept="image/*">
                 <label for="uploadBtn"><i class="fa-solid fa-upload"></i>プロフィール写真選択</label>
             </div>
-            <img id="imagePreview" alt="プレビュー">
+            <div id="croppieContainer"></div>
+            <img id="user_icon" alt="プレビュー">
+            <button type="button" id="cropBtn">切り取る</button>
             <button type="submit" class="btn">登録</button>
             <div class="register-link">
                 <?php if (!empty($errors)) : ?>
